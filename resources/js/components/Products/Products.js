f0bba8330a0d60af8c48bc56d04a0ad172a051c3
kleidilamka <kleidilamka@gmail.com>
2022-05-27 13:02:52 +0200

    Fixed some products styles

--- resources/js/components/Products/Products.js
+++ resources/js/components/Products/Products.js
@@ -5,14 +5,14 @@ import styles from "./Products.module.scss";
 const Products = () => {
     return (
         <div class={styles.root}>
-            <h1 style={{ color: "#F89B1B" }}>
+            <h1 class={`${styles.orangeTitle} ${styles.font}`}>
                 FÜR JEDEN GESCHMACK DAS RICHTIGE DABEI.
             </h1>
-            <h1 class={styles.title}>
+            <h1 class={`${styles.title} ${styles.font}`}>
                 MACHET MIT UNS <br />
                 EINE KULINARISCHE WELTREISE.
             </h1>
-            <h5 class={styles.text}>
+            <h5 class={`${styles.text} ${styles.font}`}>
                 Über 60 Menüs aus aller Herren Länder verteilt über viele
                 Kontinente.
                 <br /> So lecker kann schnelles Essen sein!
