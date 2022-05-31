976c7986ccdd6e39fa4ba2597e39e497b556d9ac
kleidilamka <kleidilamka@gmail.com>
2022-05-31 14:47:36 +0200

    Navbar for mobile finished

--- resources/js/components/Navbar/Navbar.js
+++ resources/js/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.scss";
 import Logo from "../../assets/logo_new.png";
 import HeartIconOrange from "../../assets/heartIconOrange.svg";
@@ -7,8 +7,11 @@ import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import LoginIcon from "../../assets/loginIcon.svg";
 import CartIcon from "../../assets/cartNavIcon.svg";
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
     return (
         <div class={styles.root}>
             <Link to="/" class={styles.removeUnderline}>
@@ -16,6 +19,13 @@ const Navbar = () => {
             </Link>
             {/* First Nav Row */}
             <div class={styles.firstRow}>
+                <GiHamburgerMenu
+                    class={styles.hamburgerMenu}
+                    color={"#fff"}
+                    size={30}
+                    onClick={() => setOpenMenu(true)}
+                />
+
                 <div class={styles.titleContainer}>
                     <div style={{ display: "flex", flexDirection: "row" }}>
                         <img src={HeartIconOrange} class={styles.heartIcon} />
@@ -47,72 +57,156 @@ const Navbar = () => {
                 </div>
             </div>
             {/* Second Nav Row */}
-            <div class={styles.secondRow}>
-                <div class={styles.navContainer}>
-                    <Link to="/" class={styles.removeUnderline}>
-                        <div
-                            class={styles.firstNavItem}
-                            style={{
-                                display: "flex",
-                                flexDirection: "row",
-                                position: "relative",
-                            }}
+
+            {openMenu ? (
+                <div class={styles.menu}>
+                    <div class={styles.menuContainer}>
+                        <Link
+                            to="/"
+                            class={styles.removeUnderline}
+                            onClick={() => setOpenMenu(false)}
                         >
                             <h2 class={`${styles.navTitle} ${styles.font}`}>
                                 ÜBER LUNCH4YOU
                             </h2>
-                            <MdOutlineKeyboardArrowDown size={20} />
-                        </div>
-                    </Link>
-                    <Link to="/menus" class={styles.removeUnderline}>
-                        <div>
-                            <h2 class={`${styles.navTitle} ${styles.font}`}>
-                                UNSERE MENÜS
-                            </h2>
-                        </div>
-                    </Link>
-                    <Link to="/lieferanten" class={styles.removeUnderline}>
-                        <div>
-                            <h2 class={`${styles.navTitle} ${styles.font}`}>
-                                LIEFERANTEN
-                            </h2>
-                        </div>
-                    </Link>
-                    <Link to="/koche" class={styles.removeUnderline}>
-                        <div>
-                            <h2 class={`${styles.navTitle} ${styles.font}`}>
-                                KÖCHE
-                            </h2>
-                        </div>
-                    </Link>
-                    <Link to="/blog" class={styles.removeUnderline}>
-                        <div>
-                            <h2 class={`${styles.navTitle} ${styles.font}`}>
-                                BLOG
-                            </h2>
-                        </div>
-                    </Link>
-                    <Link to="/faq" class={styles.removeUnderline}>
-                        <div>
-                            <h2 class={`${styles.navTitle} ${styles.font}`}>
-                                FAQ
-                            </h2>
-                        </div>
-                    </Link>
+                        </Link>
+                        <Link
+                            to="/menus"
+                            class={styles.removeUnderline}
+                            onClick={() => setOpenMenu(false)}
+                        >
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    UNSERE MENÜS
+                                </h2>
+                            </div>
+                        </Link>
+                        <Link
+                            to="/lieferanten"
+                            class={styles.removeUnderline}
+                            onClick={() => setOpenMenu(false)}
+                        >
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    LIEFERANTEN
+                                </h2>
+                            </div>
+                        </Link>
+                        <Link
+                            to="/koche"
+                            class={styles.removeUnderline}
+                            onClick={() => setOpenMenu(false)}
+                        >
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    KÖCHE
+                                </h2>
+                            </div>
+                        </Link>
+                        <Link
+                            to="/blog"
+                            class={styles.removeUnderline}
+                            onClick={() => setOpenMenu(false)}
+                        >
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    BLOG
+                                </h2>
+                            </div>
+                        </Link>
+                        <Link
+                            to="/faq"
+                            class={styles.removeUnderline}
+                            onClick={() => setOpenMenu(false)}
+                        >
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    FAQ
+                                </h2>
+                            </div>
+                        </Link>
 
-                    <div>
-                        <h2 class={`${styles.navTitle} ${styles.font}`}>
-                            KONTAKT
-                        </h2>
+                        <div>
+                            <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                KONTAKT
+                            </h2>
+                        </div>
                     </div>
+                    <Link to="/checkout" onClick={() => setOpenMenu(false)}>
+                        <img src={LoginIcon} class={styles.navIconOne} />
+                    </Link>
+                    <Link to="/checkout" onClick={() => setOpenMenu(false)}>
+                        <img src={CartIcon} class={styles.navIcon} />
+                    </Link>
                 </div>
-                <Link to="/checkout">
-                    <img src={LoginIcon} class={styles.navIconOne} />
-                </Link>
-                <Link to="/checkout">
-                    <img src={CartIcon} class={styles.navIcon} />
-                </Link>
-            </div>
+            ) : (
+                <div class={styles.secondRow}>
+                    <div class={styles.navContainer}>
+                        <Link to="/" class={styles.removeUnderline}>
+                            <div
+                                class={styles.firstNavItem}
+                                style={{
+                                    display: "flex",
+                                    flexDirection: "row",
+                                    position: "relative",
+                                }}
+                            >
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    ÜBER LUNCH4YOU
+                                </h2>
+                                <MdOutlineKeyboardArrowDown size={20} />
+                            </div>
+                        </Link>
+                        <Link to="/menus" class={styles.removeUnderline}>
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    UNSERE MENÜS
+                                </h2>
+                            </div>
+                        </Link>
+                        <Link to="/lieferanten" class={styles.removeUnderline}>
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    LIEFERANTEN
+                                </h2>
+                            </div>
+                        </Link>
+                        <Link to="/koche" class={styles.removeUnderline}>
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    KÖCHE
+                                </h2>
+                            </div>
+                        </Link>
+                        <Link to="/blog" class={styles.removeUnderline}>
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    BLOG
+                                </h2>
+                            </div>
+                        </Link>
+                        <Link to="/faq" class={styles.removeUnderline}>
+                            <div>
+                                <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                    FAQ
+                                </h2>
+                            </div>
+                        </Link>
+
+                        <div>
+                            <h2 class={`${styles.navTitle} ${styles.font}`}>
+                                KONTAKT
+                            </h2>
+                        </div>
+                    </div>
+                    <Link to="/checkout">
+                        <img src={LoginIcon} class={styles.navIconOne} />
+                    </Link>
+                    <Link to="/checkout">
+                        <img src={CartIcon} class={styles.navIcon} />
+                    </Link>
+                </div>
+            )}
         </div>
     );
 };
